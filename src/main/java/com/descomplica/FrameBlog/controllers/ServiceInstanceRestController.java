c8bd3653b107fcf6eaaded902bc1ce016801661a
Alexander <alexandertra25@gmail.com>
2025-04-05 20:34:17 -0300

    Adicionando Eureka no projeto

--- src/main/java/com/descomplica/FrameBlog/controllers/ServiceInstanceRestController.java
+++ src/main/java/com/descomplica/FrameBlog/controllers/ServiceInstanceRestController.java
@@ -1,2 +1,25 @@
-package com.descomplica.FrameBlog.controllers;public class ServiceInstanceRestController {
+package com.descomplica.FrameBlog.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("/service-instances")
+class ServiceInstanceRestController {
+
+    private DiscoveryClient discoveryClient;
+    public ServiceInstanceRestController(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
+
+    @RequestMapping("${spring.application.name}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
 }
